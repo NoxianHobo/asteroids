bdf57d206a12e3ae77229ae83e6b6e96bd6f4409
NoxianHobo <orjan97@gmail.com>
2026-04-11 22:39:42 +0200

    started the main game loop

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import pygame
+from logger import log_state
 from constants import *
 
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            pass
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
